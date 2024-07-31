b3eb5511ced5661c18c6882c2ad1a52989745ed3
lhakpa75 <lhakpa@tumen.nyc>
2024-07-31 15:49:53 -0400

    Task-8

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import CardGrid from "./components/cardgrid";
+import Hero from "./components/herosection";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <CardGrid />
+    </>
+  );
+}
+
+export default App;
